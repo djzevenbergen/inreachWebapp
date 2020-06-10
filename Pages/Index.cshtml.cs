0ffc8200cc0db3202d2cb7f40812b1409b86d5b7
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-06-09 20:22:26 -0700

    get uploads to save to uploads folder in local storage

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Threading.Tasks;
+namespace InreachWebapp.Pages
+{
+  public class UploadFileModel : PageModel
+  {
+    private IHostingEnvironment _environment;
+    public UploadFileModel(IHostingEnvironment environment)
+    {
+      _environment = environment;
+    }
+    [BindProperty]
+    public IFormFile Upload { get; set; }
+    public async Task OnPostAsync()
+    {
+      var file = Path.Combine(_environment.ContentRootPath, "uploads", Upload.FileName);
+      using (var fileStream = new FileStream(file, FileMode.Create))
+      {
+        await Upload.CopyToAsync(fileStream);
+      }
+    }
+  }
+}
