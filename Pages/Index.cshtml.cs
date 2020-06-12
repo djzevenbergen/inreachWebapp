e44617f6b229d3cc439f3342187cc6c2597a60e8
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-06-11 22:38:24 -0700

    add dockerization

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -25,7 +25,7 @@ namespace InreachWebapp.Pages
   public class UploadFileModel : PageModel
   {
     private const string bucketName = "inreachapplicationtest";
-    // private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
+    private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
     private static IAmazonS3 s3Client;
     public static string URLString { get; set; }
     public static string UserEmail { get; set; }
@@ -38,10 +38,13 @@ namespace InreachWebapp.Pages
     public IFormFile Upload { get; set; }
     public async Task OnPostAsync(string email)
     {
+      Console.WriteLine(email);
       UserEmail = email;
-      s3Client = new AmazonS3Client();
+      s3Client = new AmazonS3Client(bucketRegion);
       var url = GeneratePresignedUrl();
-      UploadObject(url[0]);
+      Console.WriteLine(url[0]);
+      Console.WriteLine(url[1]);
+      await UploadObject(url[0]);
       SendMail(url[1]);
     }
     private async Task UploadObject(string url)
@@ -54,15 +57,18 @@ namespace InreachWebapp.Pages
       using (FileStream stream = new FileStream(file, FileMode.Create))
       {
         await Upload.CopyToAsync(stream);
+        Console.WriteLine("File hosted in docker");
       }
       using (var fileToUpload =
           new FileStream(file, FileMode.Open, FileAccess.Read))
       {
         await fileTransferUtility.UploadAsync(fileToUpload,
                                    bucketName, Upload.FileName);
+        Console.WriteLine("File uploaded to s3");
       }
       FileInfo fileToDelete = new FileInfo(file);
       fileToDelete.Delete();
+      Console.WriteLine("File deleted");
     }
 
     private string[] GeneratePresignedUrl()
@@ -90,13 +96,13 @@ namespace InreachWebapp.Pages
       string url2 = s3Client.GetPreSignedURL(request2);
       urls[0] = url;
       urls[1] = url2;
-      Console.WriteLine(url2);
+      // Console.WriteLine(url2);
       return urls;
     }
     protected void SendMail(string url)
     {
 
-      using (var sesClient = new AmazonSimpleEmailServiceClient())
+      using (var sesClient = new AmazonSimpleEmailServiceClient(bucketRegion))
       {
         var sendRequest = new SendEmailRequest
         {
